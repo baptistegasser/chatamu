ccc38cf7c31f46a0a44288ff5d5777faeffd3d6b
lucco <luc.bobo@etu.univ-amu.fr>
2020-03-11 18:00:04 +0100

    From readline to byte buffers

--- src/Client.java
+++ src/Client.java
@@ -8,31 +8,31 @@ public class Client {
     final int port;
     private Socket socket;
     private BufferedWriter out;
-    private BufferedReader inServer;
-    private BufferedReader inClient;
 
     public Client(String adress) throws IOException {
         port = ChatamuProtocol.DEFAULT_PORT;
         socket = new Socket(adress, port);
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        inServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        inClient = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public void launch()  {
         try {
             System.out.println("Hello. Please connect.");
             System.out.println("Enter your name : ");
-            String connexion = inClient.readLine().trim();
-            out.write(ChatamuProtocol.PREFIX_LOGIN + connexion + "\n");
+            byte[] clientBuffer = new byte[256];
+            System.in.read(clientBuffer);
+            String reponse = new String(clientBuffer);
+            out.write(ChatamuProtocol.PREFIX_LOGIN + reponse + "\n");
             out.flush();
             System.out.println("Waiting for server ...");
             while(true) {
-                String reponse = inServer.readLine().trim();
+                byte[] serverBuffer = new byte[256];
+                socket.getInputStream().read(serverBuffer);
+                reponse = new String(serverBuffer).trim();
                 if(reponse.equals(ChatamuProtocol.Error.ERROR_LOGIN)) throw new IOException("Error while connecting");
                 else if (reponse.equals(ChatamuProtocol.LOGIN_SUCCESS)) break;
             }
-            new Thread(new HandlerReceived(inServer)).start();
+            new Thread(new HandlerReceived()).start();
             communication();
             closeAll();
 
@@ -43,8 +43,6 @@ public class Client {
 
     public void closeAll() throws IOException {
         socket.close();
-        inServer.close();
-        inClient.close();
         out.close();
     }
 
@@ -53,7 +51,9 @@ public class Client {
         String message;
         while (true) {
             try {
-                message = inClient.readLine().trim();
+                byte[] clientBuffer = new byte[256];
+                System.in.read(clientBuffer);
+                message = new String(clientBuffer).trim();
                 out.write(ChatamuProtocol.PREFIX_MESSAGE + message + "\n");
                 out.flush();
                 if(message.equals("quit") || message.equals("QUIT")) break;
@@ -76,19 +76,16 @@ public class Client {
     }
 
     class HandlerReceived implements Runnable {
-        private BufferedReader reader;
-
-        HandlerReceived(BufferedReader reader) {
-           this.reader = reader;
-        }
 
         public void run () {
             String messageRecv;
             while(true) {
                 try {
-                    messageRecv = reader.readLine();
+                    byte[] serverBuffer = new byte[256];
+                    socket.getInputStream().read(serverBuffer);
+                     messageRecv = new String(serverBuffer).trim();
                     System.out.println(messageRecv);
-                    if(inServer.readLine().trim().equals(ChatamuProtocol.Error.ERROR_MESSAGE))  throw new IOException("Error while sending message.");
+                    if(messageRecv.equals(ChatamuProtocol.Error.ERROR_MESSAGE))  throw new IOException("Error while sending message.");
                 } catch (IOException e) {
                     System.out.println("Bye !");
                     break;
