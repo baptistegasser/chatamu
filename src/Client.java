53012bedbb7535334a079c13938657d336a0c74b
baptistegasser <baptistegasser36@gmail.com>
2020-03-17 17:21:24 +0100

    Refactoring and cleaning and doc

--- src/Client.java
+++ src/Client.java
@@ -2,93 +2,139 @@ import protocol.ChatamuProtocol;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    private Socket socket;
-    private InputStream inputStream;
-    private OutputStream outputStream;
+    private final Socket socket;
+    private final InputStream inputStream;
+    private final OutputStream outputStream;
 
-    public Client(String adress, int port) throws IOException {
-        socket = new Socket(adress, port);
-        this.inputStream = socket.getInputStream();
-        this.outputStream = socket.getOutputStream();
+    public Client(String address, int port) throws IOException {
+        this.socket = new Socket(address, port);
+        this.inputStream = this.socket.getInputStream();
+        this.outputStream = this.socket.getOutputStream();
     }
 
+    /**
+     * Send a valid chatamu login message
+     * @param pseudo The pseudo to use in the chat
+     */
     private boolean login(String pseudo) throws IOException {
         final byte[] msg = (ChatamuProtocol.PREFIX_LOGIN + pseudo.trim()).getBytes();
         outputStream.write(msg);
         outputStream.flush();
 
         byte[] buf = new byte[ChatamuProtocol.BUFFER_SIZE];
-        inputStream.read(buf);
+        int size = inputStream.read(buf);
 
         final String resp = new String(buf).trim();
         return resp.equals(ChatamuProtocol.LOGIN_SUCCESS);
     }
 
+    /**
+     * Send a valid chatamu logout message
+     */
     private void logout() throws IOException {
         outputStream.write(ChatamuProtocol.LOGOUT_MESSAGE.getBytes());
     }
 
+    /**
+     * Send a valid chatamu message
+     * @param content The content of the message
+     */
     private void sendMessage(String content) throws IOException {
         final byte[] msg = (ChatamuProtocol.PREFIX_MESSAGE + content.trim()).getBytes();
         outputStream.write(msg);
         outputStream.flush();
     }
 
-    public void launch()  {
+    public void launch() throws IOException {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello. Please connect.");
+        System.out.print("Enter your name : ");
+
+        // Read the user pseudo and logging
+        boolean success = login(scanner.next().trim());
+        if (success) {
+            System.out.println("You're now connected and you can start tchating !");
+        } else {
+            throw new IOException("Failed to login !");
+        }
+
+        // Start the server response handler.
+        new Thread(new HandlerReceived()).start();
+
+        // Loop while user have message to send
+        String message;
+        while (true) {
+            message = scanner.next().trim();
+            if (message.toLowerCase().equals("quit")) {
+                break;
+            } else {
+                sendMessage(message);
+            }
+        }
+
+        // Logout from server
+        logout();
+    }
+
+    /**
+     * Close the open socket, streams...
+     */
+    public void closeAll() {
         try {
-
-            System.out.println("Hello. Please connect.");
-            System.out.println("Enter your name : ");
-
-            // Envoi du pseudo
-            byte[] clientBuffer = new byte[256];
-            System.in.read(clientBuffer);
-            String pseudo = new String(clientBuffer).trim();
-
-            boolean succes = login(pseudo);
-            if(!succes) throw new IOException("Failed to login !");
-
-            // Thread pour la lecture des messages
-            new Thread(new HandlerReceived()).start();
-            communication();
-            closeAll();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (!socket.isClosed()) socket.close();
+        } catch (IOException e) {
+            // Ignore the exception when closing
         }
     }
 
-    public void communication()  {
-        System.out.println("You're now connected and you can start tchating !");
-        String message;
-        while (true) {
+    /**
+     * Handler charged of reading the message sent by the server
+     */
+    class HandlerReceived implements Runnable {
+        public void run () {
             try {
-                // Envoi de messages
-                byte[] clientBuffer = new byte[256];
-                System.in.read(clientBuffer);
-                message = new String(clientBuffer).trim();
-                if(message.toLowerCase().equals("quit")) {
-                    logout();
-                    break;
-                }
-                sendMessage(message);
-            }  catch (Exception e) {
+                handle();
+            } catch (Exception e) {
+                System.out.println("Handler failed, see stack trace below:");
                 e.printStackTrace();
             }
         }
-    }
-    public void closeAll() throws IOException {
-        socket.close();
-        outputStream.close();
-        inputStream.close();
+
+        private void handle() throws Exception {
+            String response;
+            while (true) {
+                // Read a message from the server, if nothing was read, sleep 100ms
+                byte[] buf= new byte[256];
+                if (inputStream.read(buf) > 0) {
+                    response = new String(buf).trim();
+
+                    // Verify the response is not an error
+                    if (response.equals(ChatamuProtocol.Error.ERROR_MESSAGE)) {
+                        throw new IOException("Error while sending message.");
+                    } else {
+                        System.out.println(response);
+                    }
+                } else {
+                    Thread.sleep(100);
+                }
+            }
+        }
     }
 
+    /**
+     * Create a new client and start it
+     * @param args Optional arguments
+     */
     public static void main(String[] args) {
+        // Default value for the client connection
         int port = ChatamuProtocol.DEFAULT_PORT;
         final String adress = "localhost";
 
+        // Parse arguments if any
         if (args.length >= 2) {
             if (args[0].equals("--port")) {
                 try {
@@ -100,49 +146,26 @@ public class Client {
             }
         }
 
-        System.out.printf("Connecting to ChatAMU server at %s:%s%n", adress, port);
+        // Start the client and handle error
+        Client client = null;
         try {
-            Client client = new Client(adress, port);
+            client = new Client(adress, port);
             client.launch();
         } catch (Exception e) {
             System.err.println("Unexpected client failure, see stack trace below:");
             e.printStackTrace();
+        } finally {
+            // Clean all
+            if (client != null) client.closeAll();
         }
     }
 
+    /**
+     * Show how to use this program
+     */
     private static void showUsage() {
         System.out.println("Correct usage of this client:");
         System.out.println("java Client [--port n]");
         System.out.printf("--port: n specify the target port, default to %d%n", ChatamuProtocol.DEFAULT_PORT);
-    }
-
-    class HandlerReceived implements Runnable {
-
-        public void run () {
-            String response;
-
-            try {
-                while (true) {
-                    // Read a message from the server, if nothing was read, sleep 100ms
-                    byte[] buf = new byte[256];
-                    if (inputStream.read(buf) > 0) {
-                        response = new String(buf).trim();
-
-                        // Verify the response is not an error
-                        if (response.equals(ChatamuProtocol.Error.ERROR_MESSAGE)) {
-                            throw new IOException("Error while sending message.");
-                        } else {
-                            System.out.println(response);
-                        }
-                    } else {
-                        Thread.sleep(100);
-                    }
-                }
-            } catch (IOException | InterruptedException e) {
-                System.out.println("Handler failed, see stack trace below:");
-                e.printStackTrace();
-            }
-        }
-
     }
 }
